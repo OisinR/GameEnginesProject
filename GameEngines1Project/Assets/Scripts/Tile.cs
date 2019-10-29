f8547eb7ae1a58a0518fdc259502481db2e03c75
Oisin <C17343216@mydit.ie>
2019-10-29 15:22:29 +0000

    Saftey

---
+++ GameEngines1Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // in Tile
+    public int x, y;
+    public List<Tile> neighbors;
+    void Start()
+    {
+        //will run after all tiles have been generated
+        transform.position = new Vector3(x, 0, y);
+        neighbors = new List<Tile>();
+        if (x > 0) neighbors.Add(TileGenerator.tileGrid[x - 1, y]);
+        if (x < TileGenerator.width - 1) neighbors.Add(TileGenerator.tileGrid[x + 1, y]);
+        if (y > 0) neighbors.Add(TileGenerator.tileGrid[x, y - 1]);
+        if (y < TileGenerator.height - 1) neighbors.Add(TileGenerator.tileGrid[x, y + 1]);
+
+    }
+
+
+}
---
+++ GameEngines1Project/Assets/Scripts/TileGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    //these are static so that Tile can easily access them
+    public static Tile[,] tileGrid;
+    public static int width = 100;
+    public static int height = 100;
+
+    public GameObject tilePrefab;
+
+
+    void Awake()
+    {
+        tileGrid = new Tile[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject newTileGO = Instantiate(tilePrefab);
+                Tile newTile = newTileGO.GetComponent<Tile>();
+                tileGrid[x, y] = newTile;
+                newTile.x = x;
+                newTile.y = y;
+            }
+        }
+
+    }
+}
