444f52fe490717a8278e271a75f308ab2903d506
Oisin <C17343216@mydit.ie>
2019-12-14 15:20:50 +0000

    Buffers

--- GameEngines1Project/Assets/Scripts/AudioAnalyzer2.cs
+++ GameEngines1Project/Assets/Scripts/AudioAnalyzer2.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [RequireComponent (typeof (AudioSource))]
 public class AudioAnalyzer2 : MonoBehaviour
 {
     AudioSource speaker;
     public static float[] samples = new float[512];
-
+    public static float[] freqBand = new float[8];
+    public static float[] bandBuffer = new float[8];
+    float[] bufferDecrease = new float[8];
 
     void Start()
     {
@@ -18,11 +21,56 @@ public class AudioAnalyzer2 : MonoBehaviour
     void Update()
     {
         GetSpectrumData();
+        MakeFreqBands();
+        BandBuffer();
     }
 	
     void GetSpectrumData()
     {
         speaker.GetSpectrumData(samples, 0, FFTWindow.Blackman);
     }
+
+    void MakeFreqBands()
+    {
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if(i == 7)
+            {
+                sampleCount += 2;
+            }
+            for(int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            freqBand[i] = average * 10;
+        }
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; g ++)
+        {
+            if(freqBand [g] > bandBuffer [g])
+            {
+                bandBuffer[g] = freqBand[g];
+                bufferDecrease[g] = 0.005f;
+            }
+            if (freqBand[g] < bandBuffer[g])
+            {
+                bandBuffer[g] -= bufferDecrease[g];
+                bufferDecrease[g] *= 1.2f;
+
+            }
+        }
+    }
+
+
 
 }
---
+++ GameEngines1Project/Assets/Scripts/ParamCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour
+{
+    public int band;
+    public float startScale, scaleMult;
+    public bool useBuffer;
+
+    void Update()
+    {
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioAnalyzer2.bandBuffer[band] * scaleMult) + startScale, transform.localScale.z);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (AudioAnalyzer2.freqBand[band] * scaleMult) + startScale, transform.localScale.z);
+        }
+    }
+	
+}
