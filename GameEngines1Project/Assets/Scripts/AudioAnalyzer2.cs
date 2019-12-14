31b4ea3dd123e79d966db15afbb2fc544aaeed0e
Oisin <C17343216@mydit.ie>
2019-12-14 14:53:49 +0000

    Refreser1

---
+++ GameEngines1Project/Assets/Scripts/AudioAnalyzer2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioAnalyzer2 : MonoBehaviour
+{
+    AudioSource speaker;
+    public static float[] samples = new float[512];
+
+
+    void Start()
+    {
+        speaker = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        GetSpectrumData();
+    }
+	
+    void GetSpectrumData()
+    {
+        speaker.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+}
