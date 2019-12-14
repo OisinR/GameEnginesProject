ec4f1fed9ff57be9851824dc9ecff3fcdafabd0f
Oisin <C17343216@mydit.ie>
2019-12-14 15:49:06 +0000

    Lighting and normalising

---
+++ GameEngines1Project/Assets/Scripts/Lighting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+    public int band;
+    public float min, max;
+    Light bulb;
+    
+    
+    void Start()
+    {
+        bulb = GetComponent<Light>();
+    }
+
+    
+    void Update()
+    {
+        bulb.intensity = (AudioAnalyzer2.bandBuffer[band] * (max - min)) + min;
+    }
+	
+}
